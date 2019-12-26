5550f31b83d75f5f76bd2d7e77c1b54ba2e46cc6
Anusha S <anusha.gsa@gmail.com>
2019-12-26 11:04:58 +0530

    Update todolist.sql

--- todolist.sql
+++ todolist.sql
@@ -6,11 +6,15 @@ deadline_date date not null,
 completed_date date
 status number not null,
 );
-insert into todo(task, by_name, deadline_date)
-values('Install_oracle',abc,to_date('01/01/2020','dd/MM/yyyy'));
+
+create sequence task_number start with 1 increment by 1;
+constraint task_number_uq unique
+insert into todo(task_number,task, by_name, deadline_date)
+values(task_number.currval,'Install_oracle','abc',to_date('01/01/2020','dd/MM/yyyy'));
                                     
-insert todo(task, by_name, deadline_date,completed_date)
-values(Install_chrome,abc,to_date('01/01/2020','dd/MM/yyyy'),to_date('02/01/2020','dd/MM/yyyy'));
-update todo values status=1 where comp_date != null;
-update todo values status=0 where comp_date = null;
+insert todo(task_number,task, by_name, deadline_date,completed_date)
+values(task_number.nextval,'Install_chrome','abc',to_date('01/01/2020','dd/MM/yyyy'),to_date('02/01/2020','dd/MM/yyyy'));
+                                                                     
+update todo set status=1 where comp_date != null;
+update todo set status=0 where comp_date = null;
 select *from todo where status = 1;
