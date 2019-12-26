3c125ee8df64fb7e0916ca803daeba8ff2f01ca0
Anusha S <anusha.gsa@gmail.com>
2019-12-26 17:31:48 +0530

    Update employee_data.sql

--- employee_data.sql
+++ employee_data.sql
@@ -12,4 +12,13 @@ create table employee_details(
 
 create sequence employee_id start with 1 increment by 1;
 
-insert into employee_details()
+insert into employee_details( employee_name, task_name, created_date, deadline_date, priority_number) 
+values ('a','Install oracle', to_date('01/12/2019','dd/MM/yyyy'), to_date('30/01/2020','dd/MM/yyyy'), 1);
+
+insert into employee_details( employee_name, task_name, created_date, deadline_date, priority_number) 
+values ('a','Install oracle', to_date('01/12/2019','dd/MM/yyyy'), to_date('30/01/2020','dd/MM/yyyy'), 1);
+
+update todo set status=0 where completed_date is not null;
+update todo set status=0 where completed_date is null;
+
+select * from employee_details order by priority_number ASC;
