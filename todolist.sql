45ff95d5b1bf5d1c8c887be3eb828cd3bd5c19c5
Anusha S <anusha.gsa@gmail.com>
2019-12-26 12:03:43 +0530

    Update todolist.sql

--- todolist.sql
+++ todolist.sql
@@ -18,7 +18,7 @@ constraint priority_pk primary key (priority);
 insert into todo(task_number, task_name, by_name, created_date, deadline_date)
 values(task_number.currval, 'Install_oracle', 'abc', to_date('01/12/2019','dd/MM/yyyy'), to_date('01/01/2020','dd/MM/yyyy'));
                                     
-insert todo(task_number, task_name, by_name, created_date, deadline_date, modified_date, completed_date)
+insert into todo(task_number, task_name, by_name, created_date, deadline_date, modified_date, completed_date)
 values(task_number.nextval, 'Install_chrome', 'de',  to_date('01/12/2019','dd/MM/yyyy'), to_date('01/01/2020','dd/MM/yyyy'), to_date('02/01/2020','dd/MM/yyyy'));
                                                                      
 update todo set status=1 where completed_date != null;
