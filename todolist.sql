436f30416ecfc072ae4b69be5ff56d484cdfabd0
Anusha S <anusha.gsa@gmail.com>
2019-12-26 10:50:12 +0530

    Update todolist.sql

--- todolist.sql
+++ todolist.sql
@@ -1,11 +1,14 @@
 create todo (
-status number,
-task varchar(50),
-by_name varchar(25)
-deadline date,
-comp_date date
+status number not null,
+task varchar2(50)not null,
+by_name varchar2(25) not null,
+deadline_date date not null,
+completed_date date
 );
-insert todo(task, by_name, deadline)
-values(Install_oracle,abc,to_date('01/01/2020','dd/mm/yyyy');
-
-select *from todo;
+insert todo(task, by_name, deadline_date)
+values(Install_oracle,abc,to_date('01/01/2020','dd/mm/yyyy'));
+insert todo(task, by_name, deadline_date,completed_date)
+values(Install_chrome,abc,to_date('01/01/2020','dd/mm/yyyy'));
+set status=1 where comp_date != null;
+set status=0 where comp_date =null;
+select *from todo where status=1;
