895313dc85aeaa13fa6d56f7145a9285eedfb37e
Anusha S <anusha.gsa@gmail.com>
2019-12-27 16:52:05 +0530

    Update online_store.sql

--- online_store.sql
+++ online_store.sql
@@ -19,14 +19,14 @@ values (1,'abc','abcd',100,'abcde',1,'qwertyuiop',0);
 insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
 values (2,'abc','abcd',120,'abcde',2,'qwertyuiop',1);
 
-insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
-values (3,'def','efgh',80,'fghij',1,'asdfghjkl',1);
+insert into books(book_id, book_name, author_name, price, publisher,category_b, active) 
+values (3,'def','efgh',80,'fghij','asdfghjkl',1);
 
-insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
-values (4,'ghi','ijkl',900,'klmno',1,'zxcvbnm',1);
+insert into books(book_id, book_name, author_name, price, publisher, category_b, active) 
+values (4,'ghi','ijkl',900,'klmno','zxcvbnm',1);
 
-insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
-values (5,'jkl','mnop',60,'pqrst',1,'qwertyuiop',1);
+insert into books(book_id, book_name, author_name, price, publisher, category_b, active) 
+values (5,'jkl','mnop',60,'pqrst','qwertyuiop',1);
 
 insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
 values (6,'mno','qrts',90,'uvwxy',1,'asdfghjkl',0);
@@ -34,14 +34,14 @@ values (6,'mno','qrts',90,'uvwxy',1,'asdfghjkl',0);
 insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
 values (7,'mno','qrst',150,'uvwxy',2,'asdfghjkl',1);
 
-insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
-values (8,'pqr','uvwx',125,'zabcd',1,'zxcvbnm',1);
+insert into books(book_id, book_name, author_name, price, publisher, category_b, active) 
+values (8,'pqr','uvwx',125,'zabcd','zxcvbnm',1);
 
-insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
-values (9,'stu','yzab',199,'efghi',1,'qwertyuiop',1);
+insert into books(book_id, book_name, author_name, price, publisher, category_b, active) 
+values (9,'stu','yzab',199,'efghi','qwertyuiop',1);
 
-insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
-values (10,'vwx','cdef',209,'jklmno',1,'asdfghjkl',1);
+insert into books(book_id, book_name, author_name, price, publisher, category_b, active) 
+values (10,'vwx','cdef',209,'jklmno','asdfghjkl',1);
 
 create table orders(
     order_id number not null primary key,
@@ -80,8 +80,8 @@ create table order_items(
     constraint order_id_fk foreign key (order_id) references orders
 );
 
-insert into order_items (item_id, order_id, quantity, status)
-values(1,1,3,'PENDING');
+insert into order_items (item_id, order_id, quantity, status)values(1, 1, 3, 'PENDING');
+
 
 /*create table stock(
     stock_number number not null,
