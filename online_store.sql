fb25be834a0dd8f82239e05d0499290f8197068e
Anusha S <anusha.gsa@gmail.com>
2019-12-27 15:36:38 +0530

    Update online_store.sql

--- online_store.sql
+++ online_store.sql
@@ -13,7 +13,34 @@ create table books(
 );
 
 insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
-values (1,'abc','asdf',120,'ghjkl',1,'qwerty',1);
+values (1,'abc','abcd',120,'abcde',1,'qwertyuiop',0);
+
+insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
+values (2,'abc','abcd',120,'abcde',2,'qwertyuiop',1);
+
+insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
+values (3,'def','efgh',120,'fghij',1,'asdfghjkl',1);
+
+insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
+values (4,'ghi','ijkl',120,'klmno',1,'zxcvbnm',1);
+
+insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
+values (5,'jkl','mnop',120,'pqrst',1,'qwertyuiop',1);
+
+insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
+values (6,'mno','qrts',120,'uvwxy',1,'asdfghjkl',0);
+
+insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
+values (7,'mno','qrst',120,'uvwxy',2,'asdfghjkl',1);
+
+insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
+values (8,'pqr','uvwx',120,'zabcd',1,'zxcvbnm',1);
+
+insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
+values (9,'stu','yzab',120,'efghi',1,'qwertyuiop',1);
+
+insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
+values (10,'vwx','cdef',120,'jklmno',1,'asdfghjkl',1);
 
 create table orders(
     order_id number not null primary key,
@@ -30,5 +57,22 @@ create table orders(
 
 insert into orders (order_id, customer_id, book_id, total_amount, quantity, status)values (1,1,1,120,1,'PENDING');
 
-select * from books;
+create table order_items(
+    order_no number not null,
+    order_id number not null,
+    quantity number not null, 
+    status varchar2(25) not null check(status in ('PENDING','COMPLETED','CANCELLED'))
+);
+
+
+
+create table stock(
+    stock_number number not null,
+    book_id number not null,
+    quantity number 
+);
+
+
+
+select * from books order by category_b asc;
 select * from orders;
