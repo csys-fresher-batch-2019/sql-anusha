9476f22a090386e60e3b885a6eb27f4b8494766f
Anusha S <anusha.gsa@gmail.com>
2019-12-27 12:54:35 +0530

    Update online_store.sql

--- online_store.sql
+++ online_store.sql
@@ -3,19 +3,17 @@ create table books(
     book_id number primary key,
     book_name varchar2(100) not null,
     author_name varchar2(50) not null,
-    price number not null ,
+    price number not null check (price>=0),
     publisher varchar2(50) not null,
     version number,
     category_b varchar2(25) not null,
-    active number,
-    constraint price_ck check (price>=0),
-    constraint active_ck check (active in (0,1))
+    active number check (active in (0,1))
 );
 
 insert into books(book_id, book_name, author_name, price, publisher, version, category_b, active) 
 values (1,'abc','asdf',120,'ghjkl',1,'qwerty',1);
 
-select * from books;
+
 
 drop table orders;
 create table orders(
@@ -24,11 +22,14 @@ create table orders(
     book_id number,
     ordered_date timestamp default sysdate not null,
     delivered_date timestamp default sysdate,
-    total_amount number not null,
-    quantity number not null,
+    total_amount number not null check(total_amount>=0),
+    quantity number not null check(quantity>=1),
     status varchar2(20) not null check(status in('PENDING','CANCELLED','NOT AVAILABLE')),
     commands varchar2(200),
     constraint book_id_fk foreign key (book_id) references books(book_id)
 );
 
+insert into orders (order_id, customer_id, book_id, total_amount, quantity, status)values (1,1,1,120,1,'PENDING');
+
+select * from books;
 select * from orders;
