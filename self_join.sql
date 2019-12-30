626e9a17b8680b7823630e7c5f5d428d3011ffd8
Anusha S <anusha.gsa@gmail.com>
2019-12-30 10:49:03 +0530

    Update self_join.sql

--- self_join.sql
+++ self_join.sql
@@ -1,15 +1,16 @@
+drop table emp;
 create table emp(
     emp_name varchar2(25),
     emp_id number not null,
-    manager number,
+    manager number
 );
 insert into emp (emp_id ,emp_name)
 values (1,'a');
 
-insert into emp (emp_id ,emp_name)
-values (1,'b',1);
+insert into emp (emp_id ,emp_name,manager)
+values (2,'b',1);
 
-insert into emp (emp_id ,emp_name)
-values (1,'c',2);
+insert into emp (emp_id ,emp_name,manager)
+values (3,'c',2);
  
-select * from users u,tasks t;
+select e1.emp_id,e1.emp_name,e2.emp_name as manager from emp e1,emp e2 where e1.manager = e2.emp_id;
