9ee5215d4c9e1d7b2908f919804f440d0cb14aac
Anusha S <anusha.gsa@gmail.com>
2019-12-31 12:36:12 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,4 @@
+drop table patient;
 drop table doctors;
 drop table departments;
 create table departments(
@@ -17,8 +18,10 @@ create table doctors(
 create table patient(
     patient_id number primary key,
     patient_name varchar2(50) not null,
+    gender char not null,
     address varchar2(150)not null,
-    phone_number number not null
+    phone_number number not null,
+    constraint gender_ck check(gender in ('M','F'))
 );
 
 insert into departments (department_id,department_name)values (1,'car');
@@ -33,10 +36,48 @@ insert into departments (department_id,department_name)values (5,'ent');
 
 insert into departments (department_id,department_name)values (6,'gy');
  
-insert into doctors (employee_id,doc_name,department_id) values(1,'a',1);
+insert into doctors (employee_id,doc_name,department_id) values(1,'a',4);
 
-insert into patient () values ();
+insert into doctors (employee_id,doc_name,department_id) values(2,'b',1);
+
+insert into doctors (employee_id,doc_name,department_id) values(3,'c',4);
+
+insert into doctors (employee_id,doc_name,department_id) values(4,'d',2);
+
+insert into doctors (employee_id,doc_name,department_id) values(5,'e',5);
+
+insert into doctors (employee_id,doc_name,department_id) values(6,'f',3);
+
+insert into doctors (employee_id,doc_name,department_id) values(7,'g',6);
+
+insert into doctors (employee_id,doc_name,department_id) values(8,'h',3);
+
+insert into doctors (employee_id,doc_name,department_id) values(9,'i',4);
+
+insert into doctors (employee_id,doc_name,department_id) values(10,'j',1);
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (1,'z','jksg12yu',9182656261, 'M');
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (2,'y','nfbhruyf7',3648573489, 'F');
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (3,'x','jkrhwguitj4hh',2768756868, 'M');
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (4,'w','jehtuirgfuruih',9858876451, 'M');
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (5,'v','h8945ty7845yhgui45hv',9754873675, 'F');
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (6,'u','uc4t784gjkbtc85',0823697765, 'F');
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (7,'t','hx43gr7cg34ug',9768854783, 'M');
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (8,'s','4837ucb',6756478657, 'M');
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (9,'r','c84b5u8bg45u',3847567867, 'F');
+
+insert into patient (patient_id, patient_name, address, phone_number, gender) values (10,'q','h8t2c45ucbutg2i',0834564657, 'M');
  
 select * from departments;
 
 select * from doctors;
+
+select * from patient;
