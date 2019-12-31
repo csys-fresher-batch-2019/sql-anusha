f333745ee47da8dc29e13872a85e1830ed336088
Anusha S <anusha.gsa@gmail.com>
2019-12-31 16:01:47 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,4 @@
+drop table lab;
 drop table patient;
 drop table doctors;
 drop table departments;
@@ -8,76 +9,96 @@ create table departments(
   );
   
 create table doctors(
-  employee_id number primary key,
-  doc_name varchar2(50) not null,
+  doctor_id number primary key,
+  doctor_name varchar2(50) not null,
   department_id number not null,
   foreign key (department_id) references departments(department_id) ,
-  constraint doc_name_uq unique (doc_name)
+  constraint doc_name_uq unique (doctor_name)
   );
   
 create table patient(
     patient_id number primary key,
     patient_name varchar2(50) not null,
+    age number not null,
+    weight number not null,
     gender char not null,
     address varchar2(150)not null,
     phone_number number not null,
-    constraint gender_ck check(gender in ('M','F'))
+    disease varchar2(50),
+    doctor_id number not null,
+    patient_type varchar2(5) not null,
+    constraint gender_ck check(gender in ('M','F')),
+    constraint patient_type_ck check(patient_type in ('IN','OUT'))
 );
 
-insert into departments (department_id,department_name)values (1,'car');
+create table lab(
+    lab_no number,
+    patient_id number,
+    weight number,
+    doctor_id number,
+    entry_date date,
+    patient_type varchar2(5),
+    amount number,
+    result_date date,
+    results varchar2(5000)
+);
 
-insert into departments (department_id,department_name)values (2,'der');
+insert into departments (department_id,department_name)values (1,'Cardiology');
 
-insert into departments (department_id,department_name)values (3,'ped');
+insert into departments (department_id,department_name)values (2,'Dermatology');
 
-insert into departments (department_id,department_name)values (4,'gm');
+insert into departments (department_id,department_name)values (3,'Pediatrician');
 
-insert into departments (department_id,department_name)values (5,'ent');
+insert into departments (department_id,department_name)values (4,'General Medicine');
 
-insert into departments (department_id,department_name)values (6,'gy');
+insert into departments (department_id,department_name)values (5,'ENT');
+
+insert into departments (department_id,department_name)values (6,'Dental');
  
-insert into doctors (employee_id,doc_name,department_id) values(1,'a',4);
+insert into doctors (doctor_id,doctor_name,department_id) values(1,'a',4);
 
-insert into doctors (employee_id,doc_name,department_id) values(2,'b',1);
+insert into doctors (doctor_id,doctor_name,department_id) values(2,'b',1);
 
-insert into doctors (employee_id,doc_name,department_id) values(3,'c',4);
+insert into doctors (doctor_id,doctor_name,department_id) values(3,'c',4);
 
-insert into doctors (employee_id,doc_name,department_id) values(4,'d',2);
+insert into doctors (doctor_id,doctor_name,department_id) values(4,'d',2);
 
-insert into doctors (employee_id,doc_name,department_id) values(5,'e',5);
+insert into doctors (doctor_id,doctor_name,department_id) values(5,'e',5);
 
-insert into doctors (employee_id,doc_name,department_id) values(6,'f',3);
+insert into doctors (doctor_id,doctor_name,department_id) values(6,'f',6);
 
-insert into doctors (employee_id,doc_name,department_id) values(7,'g',6);
+insert into doctors (doctor_id,doctor_name,department_id) values(7,'g',6);
 
-insert into doctors (employee_id,doc_name,department_id) values(8,'h',3);
+insert into doctors (doctor_id,doctor_name,department_id) values(8,'h',3);
 
-insert into doctors (employee_id,doc_name,department_id) values(9,'i',4);
+insert into doctors (doctor_id,doctor_name,department_id) values(9,'i',4);
 
-insert into doctors (employee_id,doc_name,department_id) values(10,'j',1);
+insert into doctors (doctor_id,doctor_name,department_id) values(10,'j',1);
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (1,'z','jksg12yu',9182656261, 'M');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (1, 'z', 34, 54, 'jksg12yu', 9182656261,  'F', 'ALLERGY', 4, 'IN');
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (2,'y','nfbhruyf7',3648573489, 'F');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (2, 'y', 27, 75, 'nfbh ruyf7', 3648573489, 'M', 'GENERAL CHECK-UP', 9, 'OUT');
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (3,'x','jkrhwguitj4hh',2768756868, 'M');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (3, 'x', 10, 38, 'jkrh wguitj4hh', 2768756868, 'M', 'TOOTH ACHE', 6, 'OUT');
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (4,'w','jehtuirgfuruih',9858876451, 'M');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (4, 'w', 03, 10, 'jehtu irgfuruih', 9858876451, 'M', 'FEVER', 8, 'IN');
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (5,'v','h8945ty7845yhgui45hv',9754873675, 'F');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (5, 'v', 60, 79, 'h8945ty7 845yhgui45hv', 9754873675, 'M', 'CARDIAC ARREST', 10, 'IN');
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (6,'u','uc4t784gjkbtc85',0823697765, 'F');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (6, 'u', 19, 44, 'uc4t78 4gjkbtc85', 0823697765, 'F', 'HEAD ACHE', 3, 'OUT');
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (7,'t','hx43gr7cg34ug',9768854783, 'M');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (7, 't', 33, 77, 'hx43gr7 cg34ug', 9768854783, 'F', 'CAVITY', 6, 'OUT');
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (8,'s','4837ucb',6756478657, 'M');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (8, 's', 22, 55, '4837ucb', 6756478657, 'M', 'HEARING LOSS', 5, 'OUT');
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (9,'r','c84b5u8bg45u',3847567867, 'F');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (9, 'r', 98, 70, 'c84b5u8 bg45u', 3847567867, 'F', 'COMMON COLD', 1, 'OUT');
 
-insert into patient (patient_id, patient_name, address, phone_number, gender) values (10,'q','h8t2c45ucbutg2i',0834564657, 'M');
+insert into patient (patient_id, patient_name, age, weight, address, phone_number, gender, disease, doctor_id, patient_type) values (10, 'q', 45, 68, 'h8t2c45 ucbutg2i', 0834564657, 'M', 'ATHEROSCLEROSIS', 2, 'IN');
  
 select * from departments;
 
 select * from doctors;
 
 select * from patient;
+
+select * from lab;
